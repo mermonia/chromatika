6970eee1baa7feceadc48f019465bcfd32660936
Daniel Sanso <cs.daniel.sanso@gmail.com>
2026-02-12 12:41:48 +0100

    add distance matrix and color eq. calculation

--- internal/colors/distance.go
+++ internal/colors/distance.go
@@ -9,3 +9,30 @@ func DistanceLab(a, b *Lab) float64 {
 
 	return math.Sqrt(deltaL*deltaL + deltaA*deltaA + deltaB*deltaB)
 }
+
+func DistanceMatrix(colors []*Lab) [][]float64 {
+	n := len(colors)
+
+	res := make([][]float64, n)
+
+	// Fill distance matrix
+	for i := range n {
+		res[i] = make([]float64, n)
+		res[i][i] = 0
+		for j := i + 1; j < n; j++ {
+			dist := DistanceLab(colors[i], colors[j])
+			res[i][j] = dist
+			res[j][i] = dist
+		}
+	}
+
+	return res
+}
+
+func (a *Lab) Equals(b *Lab) bool {
+	if a.L != b.L || a.A != b.A || a.B != b.B {
+		return false
+	}
+
+	return true
+}
