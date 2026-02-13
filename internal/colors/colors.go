a2b6d9cb1f613ca30ce0714c1b204d7ef1e27749
Daniel Sanso <cs.daniel.sanso@gmail.com>
2026-02-13 17:28:02 +0100

    add pixel to lab conversion

--- internal/colors/colors.go
+++ internal/colors/colors.go
@@ -2,6 +2,7 @@ package colors
 
 import (
 	"fmt"
+	"image/color"
 	"math"
 
 	"github.com/mermonia/chromatika/internal/utils"
@@ -21,6 +22,29 @@ type NRgb struct {
 
 type Xyz struct {
 	X, Y, Z float32
+}
+
+func PixelToLab(in color.Color) (*Lab, error) {
+	rgb := PixelToRGB(in)
+	lab, err := RGBtoLab(rgb)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not convert rgb to lab: %w", err)
+	}
+
+	return lab, nil
+}
+
+func PixelToRGB(in color.Color) *Rgb {
+	r, g, b, _ := in.RGBA()
+
+	out := &Rgb{
+		R: uint8(r / 257),
+		G: uint8(g / 257),
+		B: uint8(b / 257),
+	}
+
+	return out
 }
 
 func (col *NRgb) ToMatrix() *utils.Matrix {
