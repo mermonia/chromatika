c4c42f8e4857997a59f28a974404cd6afadfb00a
Daniel Sanso <cs.daniel.sanso@gmail.com>
2026-02-14 10:45:07 +0100

    add root command

---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func Execute() {
+	cmd := &cli.Command{
+		Name:                  "chromatika",
+		EnableShellCompletion: true,
+		Version:               "v0.1.0",
+		Authors: []any{
+			"Daniel Sanso <cs.daniel.sanso@gmail.com>",
+		},
+		Copyright:   "(c) 2025 Daniel Sanso",
+		Usage:       "dominant color extraction cli",
+		HideHelp:    false,
+		HideVersion: false,
+		Commands:    []*cli.Command{},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mermonia/chromatika
 
 go 1.25.5
 
-require golang.org/x/image v0.36.0
+require (
+	github.com/urfave/cli/v3 v3.6.2
+	golang.org/x/image v0.36.0
+)
