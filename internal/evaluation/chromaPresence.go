3fe83f9f1642ab6328e72888fdd91eb108ae650a
Daniel Sanso <cs.daniel.sanso@gmail.com>
2026-02-21 09:02:50 +0100

    add background color choosing

--- internal/evaluation/chromaPresence.go
+++ internal/evaluation/chromaPresence.go
@@ -3,16 +3,38 @@ package evaluation
 import "github.com/mermonia/chromatika/internal/colors"
 
 func ChoosePrimaryColor(cols []*colors.Lab, partMatrix [][]float64) *colors.Lab {
+	chosenColor := chooseFromColorPresence(
+		cols,
+		partMatrix,
+		func(a, b float64) bool {
+			return a > b
+		})
+
+	return chosenColor
+}
+
+func ChooseBackgroundColor(cols []*colors.Lab, partMatrix [][]float64) *colors.Lab {
+	chosenColor := chooseFromColorPresence(
+		cols,
+		partMatrix,
+		func(a, b float64) bool {
+			return a < b
+		})
+
+	return chosenColor
+}
+
+func chooseFromColorPresence(cols []*colors.Lab, partMatrix [][]float64, comp func(a, b float64) bool) *colors.Lab {
 	var chosenColor *colors.Lab
-	var highestEval float64
+	var bestEval float64
 
 	for i, color := range cols {
 		presence := getColorPresence(i, partMatrix)
 		chroma := color.GetChroma()
-
 		eval := presence * chroma
-		if eval > highestEval {
-			highestEval = eval
+
+		if comp(eval, bestEval) {
+			bestEval = eval
 			chosenColor = color
 		}
 	}
